02815c75103a8fee311acd4d58f2ae9b3cbcfd1b
Aiaulym Kairbayeva <goofeatmain@gmail.com>
2022-12-04 19:16:29 +0600

    Initial commit
    Co-Authored-By: _mukhammat_ <108446245+Mukhammat04@users.noreply.github.com>

--- src/js/loadCart.js
+++ src/js/loadCart.js
@@ -9,24 +9,30 @@ let subTotal = 0;
 if (localStorage.getItem("cart")) {
     let shoppingCart = JSON.parse(localStorage.getItem("cart"));
 
-    shoppingCart.forEach(item => {
-        let cartItem = JSON.parse(item);
-        price = parseInt(cartItem.price.replaceAll(" ", ""));
-        quantity = parseInt(cartItem.quantity);
-        itemCount += quantity;
-        subTotal = price * quantity;
+    console.log(shoppingCart);
+    if (shoppingCart.length == 0) {
+        document.getElementById("cart-display").style = "visibility: hidden;";
+    } else {
+        shoppingCart.forEach(item => {
+            let cartItem = JSON.parse(item);
+            price = parseInt(cartItem.price.replaceAll(" ", ""));
+            quantity = parseInt(cartItem.quantity);
+            itemCount += quantity;
+            subTotal = price * quantity;
 
-        cartRowHTML += "<tr>" +
-            "<td>" + cartItem.category + ' ' + cartItem.company + ' ' + cartItem.name + ' ' + cartItem.color + "</td>" +
-            "<td class='text-right'>" + Intl.NumberFormat("ru", {style: "currency", currency: "KZT"}).format(price) + " ₸</td>" +
-            "<td class='text-right'>" + quantity + "</td>" +
-            "<td class='text-right'>" + Intl.NumberFormat("ru", {style: "currency", currency: "KZT"}).format(subTotal) + " ₸</td>" +
-            "</tr>";
+            cartRowHTML += 
+                "<tr>" +
+                    "<td>" + cartItem.category + ' ' + cartItem.company + ' ' + cartItem.name + ' ' + cartItem.color + "</td>" +
+                    "<td class='text-center' width='15%'>" + Intl.NumberFormat("ru").format(price) + " ₸</td>" +
+                    "<td class='text-center' width='10%'>" + quantity + "</td>" +
+                    "<td class='text-center' width='15%'>" + Intl.NumberFormat("ru").format(subTotal) + " ₸</td>" +
+                "</tr>";
 
-        grandTotal += subTotal;
-    });
+            grandTotal += subTotal;
+        });
+    }
 }
 
 document.getElementById("cartTableBody").innerHTML = cartRowHTML;
 document.getElementById("itemCount").innerHTML = itemCount;
-document.getElementById("totalAmount").innerHTML = Intl.NumberFormat("ru", {style: "currency", currency: "KZT"}).format(grandTotal);
+document.getElementById("totalAmount").innerHTML = Intl.NumberFormat("ru").format(grandTotal) + " ₸";
